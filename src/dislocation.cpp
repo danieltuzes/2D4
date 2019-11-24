ee690666615e8ff633778fb81c6a6aee489e0b64
Tüzes Dániel <danieltuzes@gmail.com>
2019-11-24 21:24:37 +0100

    bugfix: namespace error

--- src/dislocation.cpp
+++ src/dislocation.cpp
@@ -1,7 +1,10 @@
 #include "dislocation.h"
 
-std::ofstream& sdddstCore::operator<<(std::ofstream& o, const DislwoB& disl)
+namespace sdddstCore
 {
-    o << disl.x << "\t" << disl.y;
-    return o;
+    std::ofstream& operator<<(std::ofstream& o, const DislwoB& disl)
+    {
+        o << disl.x << "\t" << disl.y;
+        return o;
+    }
 }
