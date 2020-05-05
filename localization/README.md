f559854b00b4b7802f221f5fe5146cae6a8089fc
Tüzes Dániel <danieltuzes@gmail.com>
2020-05-05 19:58:04 +0200

    localization

---
+++ localization/README.md
@@ -0,0 +1,17 @@
+﻿# 2D4::localization
+The purpose of this program is to tell the level of localization of a dilsocation configuration. The width of the dislocation-ensemble-band in a system seen from a plane parallel to y at x_0 can be defined as
+
+\[d\left( {x_0} \right) = \sum\limits_i {d\left( {{x_i},{x_0}} \right)} /N\]
+where $d\left( {{x_i},{x_0}} \right)$ is the distance between the *i*th dislocation and the plane at $x_i$ considering the periodic boundary conditions, and the summation in *i* goes through the dislocation indices from 1 to *N*.
+
+The lowest value of the width for all planes is a unique value and assigned to the dislocation configuration, and denoted by $d = min_{x_0 \in [0; 1)} d(x_0)$. For uniform particle distributions, the *d* is one fourth of the system size, $d = 1/4$, as well as for any odd or even periodic function (with integer wave number larger than 1). The localization is then a measure defined by $η = 1 - 4d$.
+
+## Dependencies
+ boost/program_options
+
+## Parameters
+* The program takes arguments as filenames for which the value $x_0$ and η will be printed out to stdout.
+
+## Calculating methog
+
+To make a closed formula for the width one can represent the *x* value of each dislocation on the unit circle, calculate the center of mass and then project it onto the circe at $φ_0$. The width multiplied by 2π will given by the formula mentioned above using d to measure angles.
---
+++ localization/localization.cpp
@@ -0,0 +1,110 @@
+// localization.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+/*changelog
+# 0.1
+* first version working with the initial idea, seems working
+
+*/
+
+
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <numeric>
+#define _USE_MATH_DEFINES
+#include <math.h>
+
+#include <boost/program_options.hpp> // to read in program call arguments
+#include <boost/program_options/options_description.hpp> // to add descriptions of the program call arguments
+namespace bpo = boost::program_options;
+
+#define VERSION_localization 0.1
+
+double periodic_dist(double a, double b)
+{
+    if (abs(a - b) < 0.5)
+        return abs(a - b);
+    else
+        return 1 - abs(a - b);
+}
+
+int main(int argc, char** argv)
+{
+    std::vector<std::string> ifnames; // to store the two positional program call arguments of the input filenames
+
+#pragma region reading in variables
+    bpo::options_description requiredOptions("Required options"); // must be set from command line or file
+
+    requiredOptions.add_options()
+        ("input-files", bpo::value<std::vector<std::string>>(&ifnames), "The input files for which the localization and its plane will be determined and printed out. No switchs are needed.");
+
+    bpo::positional_options_description positionalOptions;
+    positionalOptions.add("input-files", -1);
+
+    bpo::options_description options; // the superior container of the options
+
+    options.add(requiredOptions).add_options()
+        ("help", "show this help");
+
+    bpo::variables_map vm; // the storage for the variables
+
+    try
+    {
+        bpo::store(bpo::command_line_parser(argc, argv).options(options).positional(positionalOptions).run(), vm);
+        if (vm.count("help")) // if the user is curious 
+        {
+            std::cout << "localization (version " << VERSION_localization << ") from 2D4 - a 2D discrete dislocation dynamics simulation program toolset.\n"
+                << "Copyright (C) Dániel Tüzes <tuzes@metal.elte.hu>\n";
+            std::cout << options << std::endl;
+            exit(0);
+        }
+
+        bpo::notify(vm);
+    }
+    catch (bpo::error& e)
+    {
+        std::cerr << e.what() << std::endl;
+        exit(-1);
+    }
+
+    if (ifnames.size() < 1)
+    {
+        std::cerr << "At least 1 filename is expected. Program termiantes.\n";
+        exit(-1);
+    }
+
+#pragma endregion
+
+    std::cout << "# filename\tx_0\teta" << std::endl;
+
+    for (auto ifname : ifnames)
+    {
+        std::ifstream i(ifname);
+        if (!i)
+        {
+            std::cerr << "# Warning: cannot open " << ifname << ". Program skips this file." << std::endl;
+            continue;
+        }
+        std::vector<double> x_vals;
+        for (double tmp, x; i >> x && i >> tmp && i >> tmp; )
+            x_vals.push_back(x);
+
+        double X;                                   // the center of weight for the x coordinates distributed on the unit circle
+        double Y;                                   // the center of weight for the y coordinates distributed on the unit circle
+        X = std::accumulate(x_vals.begin(), x_vals.end(), 0., [](double sum, double x) {return sum + sin(2 * M_PI * x); }) / x_vals.size();
+        Y = std::accumulate(x_vals.begin(), x_vals.end(), 0., [](double sum, double x) {return sum - cos(2 * M_PI * x); }) / x_vals.size();
+
+        //double a = sqrt(X * X + Y * Y);           // the distance of the center of mass from the unit circle's circumference, also a type of measure for localization
+        double alpha = atan2(Y, X);                 // the center of mass, origin, x axis angle
+
+        double P = (alpha + M_PI / 2) / (2 * M_PI); // the center of mass projected to the unit circle
+        double d;                                   // the sum of the distances from the plane P to all dislocations
+        d = std::accumulate(x_vals.begin(), x_vals.end(), 0., [P](double sum, double x) {return sum + periodic_dist(P, x); }) / x_vals.size();
+
+        double eta = 1 - 4 * d;
+
+        std::cout << ifname << "\t" << P << "\t" << eta << "\n";
+    }
+    return 0;
+}
